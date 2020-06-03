b645484d5c4d36366cc27c9449ba21e7b27d342a
Ishaan Puniani <ishaan_puniani@yahoo.com>
2020-06-03 13:02:58 +0200

    auth works on mobile and web

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC48Rb0PsqGzK3zQEFgcIkj2UYOB37I59I',
+  authDomain: 'api-project-971370281427.firebaseapp.com',
+  databaseURL: 'https://api-project-971370281427.firebaseio.com',
+  projectId: 'api-project-971370281427',
+  storageBucket: 'api-project-971370281427.appspot.com',
+  messagingSenderId: '971370281427',
+  appId: '1:971370281427:web:f7c73e2c04d9c54a101726',
+  measurementId: 'G-DEBL6DDL0D',
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,21 @@
 import React, {memo} from 'react';
 import {AppRegistry, View, Text, Platform, StyleSheet} from 'react-native';
+import Firebase from 'firebase';
+import {firebaseConfig} from './config/config';
 
+let app = Firebase.initializeApp(firebaseConfig);
+
+// All future sign-in request now include tenant ID.
+Firebase.auth()
+  .signInWithEmailAndPassword('ishaan.puniani@gmail.com', '123456')
+  .then(function(result) {
+    console.log(result);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
+export const db = app.database();
 const appName = 'experiment_rnw';
 const styles = StyleSheet.create({
   container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
@@ -16,6 +31,12 @@ export const Main = memo(() => {
 
 AppRegistry.registerComponent(appName, () => Main);
 if (Platform.OS === 'web') {
+  /// since analytics so far is available for web only and not working on mobile like this
+  // https://www.cardinalpath.com/blog/tracking-react-native-mobile-apps-using-google-analytics-for-firebase
+  // app.analytics();
+  // app.analytics().logEvent('first_open');
+  // app.analytics().logEvent('App Started:' + Platform.OS);
+
   AppRegistry.runApplication(appName, {
     rootTag: document.getElementById('root'),
   });
